653265f6f26e5979cb85bea24e5c1ccfe50a8b4a
shiwangi03 <madhu325.sa@gmail.com>
2025-06-06 16:20:54 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,41 +3,57 @@ import React, { Component } from 'react';
 import NavBar from './component/NavBar';
 import News from './component/News';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import LoadingBar from "react-top-loading-bar";
 export default class App extends Component {
+apiKey = process.env.React_APP_NEWS_API_KEY
+state={
+  progress:0
+}
+
+
+setProgress=(progress)=>{
+  this.setState({progress:progress})
+}
+
   render() {
     return (
       <div>
+        <LoadingBar
+        height={3}
+        color="#f11946"
+        progress={this.state.progress}
+      />
         <BrowserRouter>
+
           <NavBar />
           <Routes>
             <Route
               path="/"
-              element={<News key="general" pageSize={9} country="us" category="general" />}
+              element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="general" pageSize={9} country="us" category="general" />}
             />
             <Route
               path="/business"
-              element={<News key="business" pageSize={9} country="us" category="business" />}
+              element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="business" pageSize={9} country="us" category="business" />}
             />
             <Route
               path="/entertainment"
-              element={<News key="entertainment" pageSize={9} country="us" category="entertainment" />}
+              element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="entertainment" pageSize={9} country="us" category="entertainment" />}
             />
             <Route
               path="/health"
-              element={<News key="health" pageSize={9} country="us" category="health" />}
+              element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="health" pageSize={9} country="us" category="health" />}
             />
             <Route
               path="/science"
-              element={<News key="science" pageSize={9} country="us" category="science" />}
+              element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="science" pageSize={9} country="us" category="science" />}
             />
             <Route
               path="/sports"
-              element={<News key="sports" pageSize={9} country="us" category="sports" />}
+              element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="sports" pageSize={9} country="us" category="sports" />}
             />
             <Route
               path="/technology"
-              element={<News key="technology" pageSize={9} country="us" category="technology" />}
+              element={<News setProgress={this.setProgress} apiKey={this.apiKey}  key="technology" pageSize={9} country="us" category="technology" />}
             />
           </Routes>
         </BrowserRouter>
--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -6,12 +6,14 @@ export class NewsItem extends Component {
     return (
       <div>
         <div className="card">
+          <span className="badgbx position-absolute top-0 badge rounded-pill bg-danger" style={{right:'0'}}>
+                 {source}
+             </span>
           <img src={!imageUrl?"//cdn.decrypt.co/resize/1024/height/512/wp-content/uploads/2025/05/AI-decrypt-style-03-gID_7.png":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
+            
             <h5 className="card-title">{title} 
-            <span className="badgbx position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{ left: '86%', zIndex: 11 }}>
-                 {source}
-             </span></h5>
+            </h5>
             <p className="card-text">{description}</p>
              <p className="card-text"><small className="text-muted">By {!author?'Unknown':author} on {new Date(date).toGMTString()}</small></p>
             <a href={newsUrl} className="btn btn-sm btn-dark"  target='_blank' rel="noreferrer">Read More</a>
